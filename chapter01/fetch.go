2b089815b1bd53e0f6510e7c85653accfe49b1fc
geekymv <ym2011678@foxmail.com>
2022-04-24 00:38:20 +0800

    http get

---
+++ chapter01/fetch.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	for _, url := range os.Args[1:] {
+
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("fetch %s err:%v\n", url, err)
+			os.Exit(1)
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Printf("fetch %s read err:%v", url, err)
+		}
+
+		fmt.Println(string(b))
+	}
+
+}
