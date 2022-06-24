c1d0f064c85c280600f3f8e481fe0838ff5b1139
geekymv <ym2011678@foxmail.com>
2022-06-25 00:43:12 +0800

    类型断言

---
+++ chapter07/demo11_type.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+
+	var w io.Writer
+	w = os.Stdout
+
+	// x.(T) 如果 T 是具体类型，检查 x 的动态类型是否就是T
+	f := w.(*os.File)
+	fmt.Printf("%T\n", f)
+
+	b, ok := w.(*bytes.Buffer) // 使用ok 可以防止 panic
+	if ok {
+		fmt.Printf("%T\n", b)
+	}
+
+	// x.(T) 如果 T 是接口类型，检查 x 的动态类型是否满足T（也就是说判断 x 的动态类型是否实现了接口 T）
+	// 如果满足，返回的结果为接口类型 T
+	rw := w.(io.ReadWriter)
+	rw.Write([]byte("hello\n"))
+
+}
