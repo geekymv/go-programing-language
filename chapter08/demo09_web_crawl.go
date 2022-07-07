4e44ad503b1f72c07d1e7cb691e6bb69ae73e933
geekymv <ym2011678@foxmail.com>
2022-07-08 01:29:07 +0800

    for break

--- chapter08/demo09_web_crawl.go
+++ chapter08/demo09_web_crawl.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 // 发起 GET 请求，解析 HTML，并返回 HTML 中存在的链接
 func extract(url string) ([]string, error) {
-
+	fmt.Printf("url=%s\n", url)
+	time.Sleep(1 * time.Second)
 	// mock data
-	return []string{}, nil
+	return []string{"https://a.com", "https://b.com", "https://c.com"}, nil
 }
 
 func crawl(url string) []string {
@@ -23,6 +26,10 @@ func crawl(url string) []string {
 
 func main() {
 	worklist := make(chan []string)
+	// n 记录 channel 中元素个数，解决 deaklock 问题
+	var n int
+
+	n++
 	go func() {
 		worklist <- os.Args[1:]
 	}()
@@ -35,10 +42,17 @@ func main() {
 			}
 			// 设置已访问标志
 			seen[link] = true
-
+			// 访问一个url，会向 worklist 放入一个元素
+			n++
 			go func(link string) {
 				worklist <- crawl(link)
 			}(link)
+		}
+		// 取出一个元素
+		n--
+		// 判断 worklist channel 是否为空
+		if n == 0 {
+			break
 		}
 	}
 
