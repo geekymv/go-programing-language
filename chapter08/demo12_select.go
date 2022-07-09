bf96b3f89edbf428ab0f3df8b1d3afc526a44b28
geekymv <ym2011678@foxmail.com>
2022-07-09 15:18:53 +0800

    select

---
+++ chapter08/demo12_select.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+
+	abort := make(chan struct{})
+	go func() {
+		// 从标准输入中读取一个字符
+		os.Stdin.Read(make([]byte, 1))
+		abort <- struct{}{}
+	}()
+
+	tick := time.Tick(1 * time.Second)
+	for count := 10; count > 0; count-- {
+		select {
+		case <-tick:
+			fmt.Println(count)
+		case <-abort:
+			fmt.Println("abort")
+			return
+		}
+	}
+
+	fmt.Println("launch")
+}
