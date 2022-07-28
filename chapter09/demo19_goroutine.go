d6f6226821ccc38965eaf7e79cb2ea0f683d307e
geekymv <ym2011678@foxmail.com>
2022-07-28 19:08:46 +0800

    GPM具体含义

--- chapter09/demo19_goroutine.go
+++ chapter09/demo19_goroutine.go
@@ -6,6 +6,9 @@ import (
 
 /*
 Go调度模型GPM
+G:Goroutine
+P:逻辑Processor
+M:OS Thread
 https://deepu.tech/memory-management-in-golang/
 https://www.ardanlabs.com/blog/2018/08/scheduling-in-go-part2.html
 */
