391dd423e35104ae9c1b36268324988724c4078a
geekymv <ym2011678@foxmail.com>
2022-07-28 18:49:01 +0800

    GPM

--- chapter09/demo19_goroutine.go
+++ chapter09/demo19_goroutine.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+/*
+Go调度模型GPM
+https://deepu.tech/memory-management-in-golang/
+https://www.ardanlabs.com/blog/2018/08/scheduling-in-go-part2.html
+*/
+
 func main() {
 	//runtime.GOMAXPROCS(1)
 	for {
